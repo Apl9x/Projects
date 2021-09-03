ae02d5649c6da9b0393292d63bf88590dde6c754
Apl9x <44626021+Apl9x@users.noreply.github.com>
2021-09-03 10:31:15 -0500

    Add files via upload

---
+++ Proyecto final AI Reconocimiento de Rostros/entrenamiento.py
@@ -0,0 +1,46 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'C:/Users/Lenny/Desktop/yoyoni/6to/Lunita/Proyecto final AI/data'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+    personPath = dataPath + '/' + nameDir
+    print('Leyendo imagenes')
+
+    for fileName in os.listdir(personPath):
+        print('Rostros: ', nameDir + '/' + fileName)
+        labels.append(label)
+        facesData.append(cv2.imread(personPath + '/' + fileName,0))
+        image = cv2.imread(personPath + '/' + fileName,0)
+        #cv2.imshow('image', image)
+        #cv2.waitKey(10)
+    label = label + 1
+
+print('labels= ',labels)
+print('Número de etiquetas 0: ',np.count_nonzero(np.array(labels)==0))
+print('Número de etiquetas 1: ',np.count_nonzero(np.array(labels)==1))
+
+#Entrenamos EigenFaces
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+print("Entrenamos")
+face_recognizer.train(facesData, np.array(labels))
+
+#Almacenamos el modelo
+#face_recognizer.write('modeloEigenface.xml')
+#face_recognizer.write('modeloFisherface.xml')
+face_recognizer.write('modeloLBPHface.xml')
+
+print("Modelo almacenado")
+
+#cv2.destroyAllWindows()
